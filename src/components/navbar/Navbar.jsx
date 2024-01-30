68489c191bc3b55d06a85bdcbce8c72b66db900d
arkhanis <casanovac@gmail.com>
2024-01-29 22:15:39 -0300

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
 import './navbar.css';
-// import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
+const Menu = () => (
+  <>
+    <p> <a href="#home">Home</a></p>
+    <p> <a href="#how">How It Works</a></p>
+    <p> <a href="#why">Why Us</a></p>
+    <p> <a href="#testimonials">Testimonials</a></p>
+  </>
+)
 
 const Navbar = () => {
 
@@ -17,10 +25,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='pawsC__navbar-links_container'>
-          <p> <a href="#home">Home</a></p>
-          <p> <a href="#how">How It Works</a></p>
-          <p> <a href="#why">Why Us</a></p>
-          <p> <a href="#testimonials">Testimonials</a></p>
+          <Menu />
         </div>
       </div>
 
@@ -30,9 +35,21 @@ const Navbar = () => {
       </div>
 
       <div className='pawsC__navbar-menu'>
-        { toggleMenu 
-          ? <RiCloseLine color="#fff" onClick={() => setToggleMenu(!toggleMenu)} />
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
         }
+        {toggleMenu && (
+          <div className='pawsC__navbar-menu_container scale-up-center'>
+            <div className='pawsC__navbar-menu_container-links'>
+              <Menu />
+              <div className='pawC__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )

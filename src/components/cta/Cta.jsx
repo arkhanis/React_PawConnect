55faf5355937a6dc0e03d3b19fabe1c118e5235f
arkhanis <casanovac@gmail.com>
2024-02-01 10:07:36 -0300

    features

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='paws__cta'>
+      <div className='paws__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & Start the Fun!</h3>
+      </div>
+      <div className='paws__cta-btn'> 
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css'
+
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='paws__possibility section__padding' id="possibility">
+      <div className='paws__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='paws__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>Don't let your pet miss out on the fun!</h1>
+        <p>Welcome to the cutest community! Share delightful moments. Swipe right for the perfect furry friendship.</p>
+      </div>
+    </div>
   )
 }
 
-export default Possibility
+export default Possibility;

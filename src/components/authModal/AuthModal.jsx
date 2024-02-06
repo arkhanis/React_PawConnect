9461e56239bfcfe746e7ce7956ef741155dd9b82
arkhanis <casanovac@gmail.com>
2024-02-06 14:33:46 -0300

    auth modal

--- src/components/authModal/AuthModal.jsx
+++ src/components/authModal/AuthModal.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './authModal.css'
+import { useState } from 'react'
 
 const AuthModal = ({setShowModal}) => {
 
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
+
+
     const handleClick = () => {
         setShowModal(false)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
     }
 
     const isSignUp = true
@@ -15,7 +26,24 @@ const AuthModal = ({setShowModal}) => {
         
         <h2>{isSignUp ? "Create Account" : "Log In"}</h2>
         <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Private Policy and Cookies Policy</p>
-
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="email"
+            id='email'
+            name='email'
+            placeholder="Email" 
+            required={true}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password"
+            id='password'
+            name='password'
+            placeholder="Password" 
+            required={true}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
         AUTH MODAL
     </div>
   )
